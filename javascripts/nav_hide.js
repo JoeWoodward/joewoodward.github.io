270c996c46b51cd80ae94b4a5288059ab93aadbb
Joe Woodward <woodward.jp@gmail.com>
2013-06-16 23:08:40 +0100

    Site updated at 2013-06-16 22:08:40 UTC

--- javascripts/nav_hide.js
+++ javascripts/nav_hide.js
@@ -1,13 +1,35 @@
 $(window).load(function(){
+  $('#no_distractions_toggle').click( function(){
+    if($('#no_distractions').length > 0){
+      expandNav();
+    } else {
+      expandArticle();
+    }
+  });
   $(window).scroll(function(){
     if($(window).width() < 980){
       if($(window).scrollTop() > 216){
-        $('#sidebar').css({  position: 'fixed', zIndex: '10000', top: '-216px'});
+        $('#sidebar').css({  position: 'fixed', zIndex: '10000', top: '-216px', boxShadow: '0 0 10px rgba(0,0,0,0.1)'});
         $('#main').css({marginTop: 254})
       } else {
-        $('#sidebar').css({  position: '', zIndex: '1', top: ''});
+        $('#sidebar').css({  position: '', zIndex: '1', top: '', boxShadow: 'none'});
         $('#main').css({marginTop: '-1px'})
       }
     }
   });
+  $(window).resize(function(){
+    if($(window).width() < 980 && $('#no_distractions').length > 0){
+      expandNav();
+    }
+  });
 });
+
+function expandNav(){
+  $('#no_distractions').attr('id', 'main');
+  $('#no_distractions_toggle').css({left: '27%', backgroundImage: '', borderRadius: 0, borderTopLeftRadius: 5, borderBottomLeftRadius: 5, borderRight: 'none', borderLeft: '1px solid #e8e8e8'}).text('<');
+}
+
+function expandArticle(){
+  $('#main').attr('id', 'no_distractions');
+  $('#no_distractions_toggle').css({left: 30, backgroundImage: 'url(/images/line-tile.png)', borderRadius: 0, borderTopRightRadius: 5, borderBottomRightRadius: 5, borderLeft: 'none', borderRight: '1px solid #e8e8e8'}).text('>');
+}
